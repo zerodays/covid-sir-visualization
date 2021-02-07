65ab358330b38c54c0602f6ffccec5e83c625866
Ziga Patacko Koderman <ziga.patacko@gmail.com>
2021-02-07 17:59:25 +0100

    render dummy visualization with zoom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,88 @@
-import { Button } from '@material-ui/core';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core';
+import { colors } from '@material-ui/core';
+import VisualizationField from './VisualizationField';
 
-function App() {
-  return (
-    <div>
-      <Button color="primary">Hello World</Button>
-    </div>
-  );
+const RECT_SIZE = 2;
+
+const styles = theme => ({
+  root: {
+    overflow: 'hidden',
+    height: '100vh',
+    width: '100vw',
+  },
+});
+
+class App extends Component {
+  fieldRef = React.createRef();
+  state = {
+    seedCalled: false,
+    zoom: 1.0,
+    widthPoints: null,
+    heightPoints: null,
+  };
+
+  componentDidMount() {
+    const w = document.body.clientWidth;
+    const h = document.body.clientHeight;
+
+    const wPoints = Math.ceil(w / RECT_SIZE);
+    const hPoints = Math.ceil(h / RECT_SIZE);
+
+    this.setState({
+      widthPoints: wPoints,
+      heightPoints: hPoints,
+    });
+
+    console.log(wPoints, hPoints);
+  }
+
+  iterateWithSeed = (e) => {
+    // console.log(e);
+    if (e.charCode !== 13) return;
+
+    const field = this.fieldRef.current;
+
+    if (!this.state.seedCalled || true) {
+      const w = document.body.clientWidth;
+      const h = document.body.clientHeight;
+
+      const xPosition = Math.ceil(w / RECT_SIZE / 2);
+      const yPosition = Math.ceil(h / RECT_SIZE / 2);
+
+      field.addSeed(xPosition, yPosition); // TODO: seed position
+      this.setState({seedCalled: true});
+    }
+
+    field.tick();
+  };
+
+  onScroll = (e) => {
+    let newZoom = this.state.zoom + e.deltaY / 1000;
+    newZoom = Math.min(3, Math.max(1, newZoom));
+    this.setState({zoom: newZoom});
+  };
+
+  render() {
+    const {classes} = this.props;
+    return <div className={classes.root} onKeyPress={this.iterateWithSeed} tabIndex={'0'} onWheel={this.onScroll}>
+      {
+        this.state.widthPoints == null ? null :
+          <VisualizationField
+            ref={this.fieldRef}
+            widthPoints={this.state.widthPoints}
+            heightPoints={this.state.heightPoints}
+            rectSize={RECT_SIZE}
+            susceptibleColor={colors.grey[200]}
+            infectedColor={colors.red[400]}
+            removedColor={colors.blue[400]}
+            gamma={1}
+            betta={1}
+            zoom={this.state.zoom}
+          />
+      }
+    </div>;
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/VisualizationField.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  withStyles,
+} from '@material-ui/core';
+
+const styles = theme => ({
+  root: {
+    position: 'relative',
+    overflow: 'hidden',
+  },
+  canvas: {
+    position: 'absolute',
+  },
+});
+
+// global data fields for better performance
+let field;
+const SUSCEPTIBLE = 0;
+const INFECTED = 1;
+const REMOVED = 2;
+
+class VisualizationField extends Component {
+  canvasRef = React.createRef();
+
+  componentDidMount() {
+    field = this.emptyField();
+  }
+
+  countInfectedNeighbours = (field, x, y) => {
+    const {widthPoints, heightPoints} = this.props;
+
+    let count = 0;
+
+    for (let i = y - 1; i < y + 2; i++) {
+      for (let j = x - 1; j < x + 2; j++) {
+
+        if (i === x && j === y) continue; // skip self
+        if (i < 0 || j < 0 || i >= heightPoints || j >= widthPoints) continue; // watch for array boundaries
+
+        if (field[i][j] === INFECTED) count++;
+      }
+    }
+
+    return count;
+  };
+
+  paintRect = (x, y, newField, canvasContext) => {
+    const {rectSize} = this.props;
+    canvasContext.fillStyle = this.getColorFromState(newField[y][x]);
+    canvasContext.fillRect(rectSize * x, rectSize * y, rectSize, rectSize);
+  };
+
+  getNewStateForPoint = (field, x, y) => {
+    // TODO: this is a dummy logic to test the performance
+    if (field[y][x] === REMOVED) return REMOVED;
+
+    let infectedNeighbours = this.countInfectedNeighbours(field, x, y);
+
+    if (field[y][x] === INFECTED) {
+      return infectedNeighbours === 8 ? REMOVED : INFECTED;
+    }
+
+    if (infectedNeighbours > 0 && infectedNeighbours <= 3) return INFECTED;
+    return SUSCEPTIBLE;
+  };
+
+  getColorFromState = (state) => {
+    const {infectedColor, susceptibleColor, removedColor} = this.props;
+    if (state === INFECTED) return infectedColor;
+    if (state === REMOVED) return removedColor;
+    return susceptibleColor;
+  };
+
+  emptyField = () => {
+    const {widthPoints, heightPoints} = this.props;
+
+    let newField = Array(heightPoints);
+    for (let i = 0; i < heightPoints; i++) {
+      newField[i] = Array(widthPoints).fill(0);
+    }
+    return newField;
+  };
+
+  evolve = (field, canvasContext) => {
+    const {widthPoints, heightPoints} = this.props;
+    let newField = this.emptyField();
+
+    for (let y = 0; y < heightPoints; y++) {
+      for (let x = 0; x < widthPoints; x++) {
+        newField[y][x] = this.getNewStateForPoint(field, x, y);
+
+        if (newField[y][x] !== field[y][x]) this.paintRect(x, y, newField, canvasContext);
+      }
+    }
+
+    return newField;
+  };
+
+  tick = () => {
+    let context = this.getCanvasContext();
+
+    let startTime = new Date().getTime();
+    field = this.evolve(field, context);
+    console.log(`time for evolution: ${new Date().getTime() - startTime} ms`);
+  };
+
+  addSeed = (x, y) => {
+    field[y][x] = INFECTED;
+    this.paintRect(x, y, field, this.getCanvasContext());
+  };
+
+  // TODO: call reset from outside
+  reset = () => {
+    const {widthPoints, heightPoints, rectSize} = this.props;
+    field = this.emptyField();
+    this.getCanvasContext().clearRect(0, 0, widthPoints * rectSize, heightPoints * rectSize);
+  };
+
+  getCanvasContext = () => {
+    return this.canvasRef.current.getContext('2d');
+  };
+
+
+  render() {
+    const {classes, widthPoints, heightPoints, rectSize, susceptibleColor} = this.props;
+
+    return <div className={classes.root} style={{
+      width: widthPoints * rectSize,
+      height: heightPoints * rectSize,
+    }}>
+      <canvas
+        ref={this.canvasRef}
+        className={classes.canvas}
+        style={{
+          backgroundColor: susceptibleColor,
+          transform: `scale(${this.props.zoom})`,
+          width: widthPoints * rectSize,
+          height: heightPoints * rectSize,
+        }}
+        width={widthPoints * rectSize}
+        height={heightPoints * rectSize}/>
+    </div>;
+  }
+}
+
+VisualizationField.propTypes = {
+  betta: PropTypes.number.isRequired,
+  gamma: PropTypes.number.isRequired,
+  zoom: PropTypes.number.isRequired,
+  widthPoints: PropTypes.number.isRequired,
+  heightPoints: PropTypes.number.isRequired,
+  rectSize: PropTypes.number.isRequired,
+  infectedColor: PropTypes.string.isRequired,
+  susceptibleColor: PropTypes.string.isRequired,
+  removedColor: PropTypes.string.isRequired,
+};
+
+export default withStyles(styles)(VisualizationField);
+
