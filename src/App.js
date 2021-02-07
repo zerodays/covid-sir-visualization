02f0bae88c57b2832594ec90b5027fab2017242d
Ziga Patacko Koderman <ziga.patacko@gmail.com>
2021-02-07 16:06:34 +0100

    init react project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Button } from '@material-ui/core';
+
+function App() {
+  return (
+    <div>
+      <Button color="primary">Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
